112047441cda3ffdc985c7e3520d0549a33170d9
Freddie Sackur <github@dustyfox.uk>
2023-10-31 20:24:46 +0000

    Bootstrap with ansible

---
+++ Harriet-DT/ansible/run_ansible.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+dir=$(dirname $0)
+
+playbook="${1:-$(hostname)}.yml"
+if [ "$playbook" == "" ]; then
+    echo "Must specify a playbook"
+    exit 1
+fi
+shift
+
+if [ -z "$(which ansible-playbook)" ]; then
+    echo "Installing ansible..."
+    sudo apt install -y ansible
+fi
+
+_=$(sudo -n ls 2>&1)
+if [ $? -eq 0 ]; then
+    ask_become_pass=""
+else
+    ask_become_pass="--ask-become-pass"
+fi
+
+ansible-playbook \
+    $dir/$playbook \
+    -i "$(hostname)," \
+    -l "$(hostname)," \
+    -c local \
+    -u $(whoami) \
+    ${ask_become_pass} \
+    $@
