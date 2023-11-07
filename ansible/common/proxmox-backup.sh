498e129f7244332395953adc9659c1780c1991c0
Freddie Sackur <github@dustyfox.uk>
2023-11-07 00:37:12 +0000

    Proxmox backup config

---
+++ ansible/common/proxmox-backup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -euo pipefail
+err=""
+
+# env file is next to this file and has .env extension
+env_file="${0/.sh/.env}"
+if [ -f "$env_file" ]; then
+    source $env_file
+fi
+
+# default values if not overriden by env file
+passwd_file="${passwd_file:-proxmox_backup_passwd}"
+username="${username:-backup_user@pbs}"
+token_name="${token_name:-$(hostname)}"
+server="${server:-backoops}"
+datastore="${datastore:-Test}"
+
+# set by systemd, defaults to /root/.secrets
+CREDENTIALS_DIRECTORY="${CREDENTIALS_DIRECTORY:-/root/.secrets/}"
+
+export PBS_REPOSITORY="$username!$token_name@$server:443:$datastore"
+export PBS_PASSWORD_FILE="$CREDENTIALS_DIRECTORY/$passwd_file"
+
+IFS=$'\n'
+backup_target="harriet-home.pxar:/home/harriet/"
+excludes=(
+    ".cache"
+    ".Trash"
+    ".gvfs"
+    ".local/share/Trash/"
+    ".thumbnails"
+    "Downloads/"
+    ".mozilla/"
+)
+for exclude in ${excludes[@]}; do
+    backup_target+=" --exclude $exclude"
+done
+
+
+backup_targets=($backup_target)
+set +e
+timeshift_base="/timeshift/timeshift/snapshots"
+timeshift_path=$(ls $timeshift_base | tail -n1)
+if [ -z "$timeshift_path" ]; then
+    # If we couldn't parse the timeshift directory, we should still back up the main set
+    err="Failed to find latest timeshift backup."
+else
+    backup_targets+=("timeshift.pxar:$timeshift_base/$timeshift_path/")
+fi
+set -e
+
+
+for target in ${backup_targets[@]}; do
+    IFS=$' \t\n'
+    args=($target)
+    backup_spec="${args[0]}"
+    name="${backup_spec%:*}"
+    path="${backup_spec#*:}"
+    echo "=========================================="
+    echo "Backing up $path to $name ..."
+    proxmox-backup-client backup $target --skip-lost-and-found true "$@"
+done
+
+
+if [ ! -z "$err" ]; then
+    echo "$err"
+    exit 1
+fi
